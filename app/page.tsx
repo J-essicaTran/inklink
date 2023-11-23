34b263e451321dc1ea75da0e6ece033f64a8d53c
J-essicaTran <jessica.tran3@ucalgary.ca>
2023-11-23 07:45:45 +0000

    Updated app/page.txs (home page) to only allow authenticated users to access it

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,20 @@
-import Image from 'next/image'
-import Link from 'next/link'
+'use client';
+import { signOut, useSession } from 'next-auth/react';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
+  const session = useSession({
+    required: true,
+    onUnauthenticated() {
+      redirect('/signin');
+    },
+  });
   return (
-    <div className="flex justify-center gap-3 pt-5">
-      <Link href="/" className="bg-black text-white p-2 px-4 rounded-full">Home</Link>
-      <Link href="/register" className="bg-black text-white p-2 px-4 rounded-full">Register</Link>
-      <Link href="/login" className="bg-black text-white p-2 px-4 rounded-full">Login</Link>
-      <Link href="/profile" className="bg-black text-white p-2 px-4 rounded-full">Profile</Link>
-      <Link href="/create-journal" className="bg-black text-white p-2 px-4 rounded-full">Create Journal</Link>
-      <Link href="/view-journal" className="bg-black text-white p-2 px-4 rounded-full">View Journal</Link>
-      <Link href="/view-journal-image" className="bg-black text-white p-2 px-4 rounded-full">View Journal Image</Link>      
-      <Link href="/search-user" className="bg-black text-white p-2 px-4 rounded-full">Search User</Link>
-      <Link href="/admin" className="bg-black text-white p-2 px-4 rounded-full">Admin</Link>
+    <div className="p-8 pt-20">
+      <div className='text-black'>{session?.data?.user?.email }</div>
+      <button className='text-black' onClick={() => signOut()}>Logout</button>
     </div>
   )
 }
+
+Home.requireAuth = true
