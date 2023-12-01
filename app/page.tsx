d9ef1596b8a03f040acc20c37908d468d02383cf
GreenStripedSweater <reetsuni@gmail.com>
2023-11-30 22:56:20 -0700

    created the profile page and the home/landing page need to implement the buttons properly on the profile page though, landing page needs minor fixes such as making it so that when in webpage view the journal divs aren't on top of each other - will fix tomorrow

--- app/page.tsx
+++ app/page.tsx
@@ -28,53 +28,175 @@ export default function Home() {
         {/* remove navigation to test  vv*/}
         <Navigation/>
         <div className="flex flex-col items-center justify-center p-8 pt-20">
-          <div className='text-white'>Email: {session?.user?.email}</div>
-          <button className='bg-white text-black p-2 px-4 mt-5 rounded-full hover:bg-[#6b7ced]' onClick={() => signOut()}>Logout</button>
-        
-          <div className="w-screen bg-gray-200 p-12 flex flex-col sm:flex-row items-start justify-between">
+
+
+        <div>
+          <div className="w-full rounded-lg bg-neutral-800 p-12 flex flex-col sm:flex-row items-start justify-between">
             
-            {/* turn them into buttons */}
-            <div className="rounded-xl p-2 text-lg font-bold bg-blue-600 mb-2 sm:mb-0 sm:mr-4 sm:self-end">Explore</div>
-            <div className="rounded-xl p-1 text-sm text-right bg-pink-300 sm:self-start">See more</div>
+            <button type="button" className="border-2 border-purple-500 rounded-full px-5 py-2 text-purple-300 cursor-default hover:bg-purple-500 hover:text-purple-200">
+              <span>Explore</span>
+            </button>
+            
+            <button type="button" className="border-2 border-purple-500 rounded-full px-3 py-1 text-purple-300 cursor-default hover:bg-purple-500 hover:text-purple-200">
+              <span>See More</span>
+            </button>          
           </div>
 
+          <div className="h-2 w-full bg-pink-600 flex justify-center items-center rounded-full">
+            <div className="h-1 w-full bg-white rounded-full"></div>
+          </div>
 
+          <div className="w-full bg-neutral-800 p-4 rounded-lg shadow-lg flex flex-col sm:flex-row sm:flex-wrap justify-between">
+            
+            <div className="flex self-center w-48 h-48 sm:w-60 sm:h-60 bg-neutral-700 p-8 rounded-lg border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md relative z-10 mb-4 sm:mb-0 sm:mr-4">
+              <img src="" alt="" />
+             
+              <div className='absolute bottom-4 left-2 flex items-end'>
+                
+                <div className='flex flex-col'>
+                  <div className="self-start m-0 text-sm">
+                    {/* the 'Title' placeholder gets replaced with actual title  */}
+                    Title:</div>
+                  <div className="self-start m-0 text-xs">Description:</div>
 
+                </div>
+              </div>
 
+            </div>
 
+            <div className="flex self-center w-48 h-48 sm:w-60 sm:h-60 bg-neutral-700 p-8 rounded-lg border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md relative z-10 mb-4 sm:mb-0 sm:mr-4">
+              <img src="" alt="" />
+             
+              <div className='absolute bottom-4 left-2 flex items-end'>
+                
+                <div className='flex flex-col'>
+                  <div className="self-start m-0 text-sm">
+                    {/* the 'Title' placeholder gets replaced with actual title  */}
+                    Title:</div>
+                  <div className="self-start m-0 text-xs">Description:</div>
 
+                </div>
+              </div>
 
-      <div className="h-2 w-full bg-pink-600 flex justify-center items-center rounded-full">
-        <div className="h-1 w-3/4 bg-white rounded-full"></div>
-      </div>
+            </div>            <div className="flex self-center w-48 h-48 sm:w-60 sm:h-60 bg-neutral-700 p-8 rounded-lg border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md relative z-10 mb-4 sm:mb-0 sm:mr-4">
+              <img src="" alt="" />
+             
+              <div className='absolute bottom-4 left-2 flex items-end'>
+                
+                <div className='flex flex-col'>
+                  <div className="self-start m-0 text-sm">
+                    {/* the 'Title' placeholder gets replaced with actual title  */}
+                    Title:</div>
+                  <div className="self-start m-0 text-xs">Description:</div>
 
+                </div>
+              </div>
 
-      <div className="w-full h-72 p-6 bg-neutral-800 rounded-lg shadow-lg flex flex-row space-x-3">
+            </div>            <div className="flex self-center w-48 h-48 sm:w-60 sm:h-60 bg-neutral-700 p-8 rounded-lg border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md relative z-10 mb-4 sm:mb-0 sm:mr-4">
+              <img src="" alt="" />
+             
+              <div className='absolute bottom-4 left-2 flex items-end'>
+                
+                <div className='flex flex-col'>
+                  <div className="self-start m-0 text-sm">
+                    {/* the 'Title' placeholder gets replaced with actual title  */}
+                    Title:</div>
+                  <div className="self-start m-0 text-xs">Description:</div>
 
+                </div>
+              </div>
 
-        <div className="w-full h-full bg-neutral-700 p-8 text-center rounded-lg border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md" >
-          <img src="" alt="" />
+            </div>
+
+          </div>
         </div>
 
-        <div className="w-full h-full bg-neutral-700 p-8 text-center rounded-lg  border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md" >
-          <img src="" alt="" />
+        <div className="w-20 h-30 p-3">
+
         </div>
 
-        <div className="w-full h-full bg-neutral-700 p-8 text-center rounded-lg  border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md" >
-          <img src="" alt="" />
-        </div>
+        <div>
+          <div className="w-full rounded-lg bg-neutral-800 p-12 flex flex-col sm:flex-row items-start justify-between">
+            
+            <button type="button" className="border-2 border-purple-500 rounded-full px-5 py-2 text-purple-300 cursor-default hover:bg-purple-500 hover:text-purple-200">
+              <span>Your Journals</span>
+            </button>
+            
+            <button type="button" className="border-2 border-purple-500 rounded-full px-3 py-1 text-purple-300 cursor-default hover:bg-purple-500 hover:text-purple-200">
+              <span>See More</span>
+            </button>          
+          </div>
 
-        <div className="w-full h-full bg-neutral-700 p-8 text-center rounded-lg  border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md" >
-          <img src="" alt="" />
-        </div>
+          <div className="h-2 w-full bg-pink-600 flex justify-center items-center rounded-full">
+            <div className="h-1 w-full bg-white rounded-full"></div>
+          </div>
 
-        <div className="w-full h-full bg-neutral-700 p-8 text-center rounded-lg  border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md" >
-          <img src="" alt="" />
-        </div>
-      </div>
+          <div className="w-full bg-neutral-800 p-4 rounded-lg shadow-lg flex flex-col sm:flex-row sm:flex-wrap justify-between">
+            
+          <div className="flex self-center w-48 h-48 sm:w-60 sm:h-60 bg-neutral-700 p-8 rounded-lg border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md relative z-10 mb-4 sm:mb-0 sm:mr-4">
+              <img src="" alt="" />
+             
+              <div className='absolute bottom-4 left-2 flex items-end'>
+                
+                <div className='flex flex-col'>
+                  <div className="self-start m-0 text-sm">
+                    {/* the 'Title' placeholder gets replaced with actual title  */}
+                    Title:</div>
+                  <div className="self-start m-0 text-xs">Description:</div>
 
+                </div>
+              </div>
+
+            </div>            <div className="flex self-center w-48 h-48 sm:w-60 sm:h-60 bg-neutral-700 p-8 rounded-lg border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md relative z-10 mb-4 sm:mb-0 sm:mr-4">
+              <img src="" alt="" />
+             
+              <div className='absolute bottom-4 left-2 flex items-end'>
+                
+                <div className='flex flex-col'>
+                  <div className="self-start m-0 text-sm">
+                    {/* the 'Title' placeholder gets replaced with actual title  */}
+                    Title:</div>
+                  <div className="self-start m-0 text-xs">Description:</div>
+
+                </div>
+              </div>
+
+            </div>            <div className="flex self-center w-48 h-48 sm:w-60 sm:h-60 bg-neutral-700 p-8 rounded-lg border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md relative z-10 mb-4 sm:mb-0 sm:mr-4">
+              <img src="" alt="" />
+             
+              <div className='absolute bottom-4 left-2 flex items-end'>
+                
+                <div className='flex flex-col'>
+                  <div className="self-start m-0 text-sm">
+                    {/* the 'Title' placeholder gets replaced with actual title  */}
+                    Title:</div>
+                  <div className="self-start m-0 text-xs">Description:</div>
+
+                </div>
+              </div>
+
+            </div>            <div className="flex self-center w-48 h-48 sm:w-60 sm:h-60 bg-neutral-700 p-8 rounded-lg border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md relative z-10 mb-4 sm:mb-0 sm:mr-4">
+              <img src="" alt="" />
+             
+              <div className='absolute bottom-4 left-2 flex items-end'>
+                
+                <div className='flex flex-col'>
+                  <div className="self-start m-0 text-sm">
+                    {/* the 'Title' placeholder gets replaced with actual title  */}
+                    Title:</div>
+                  <div className="self-start m-0 text-xs">Description:</div>
+
+                </div>
+              </div>
+
+            </div>
+
+          </div>
+        </div>
 
         
+        <div className='text-white'>Email: {session?.user?.email}</div>
+          <button className='bg-white text-black p-2 px-4 mt-5 rounded-full hover:bg-[#6b7ced]' onClick={() => signOut()}>Logout</button>
         
         
         
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link"
 
 function Profile() {
   return (
     <div>
+      
       <div className="p-1 pb-3 flex flex-row">
         <div className="self-center flex-grow">
           <button className="pl-1">
@@ -9,33 +11,33 @@ function Profile() {
           </button>
         </div>
 
-        <button type="button" className="mr-2 border-2 border-purple-500 rounded-full px-3 py-2 text-purple-300 cursor-pointer hover:bg-purple-500 hover:text-purple-200">
-          <img src="./profile_icon.png" alt="profile icon" />
-        </button>
-
-
         {/* change to explorer page icon - make sure it takes us to the explorer page  */}
-        <button type="button" className="ml-2 mr-2 border-2 border-purple-500 rounded-full px-3 py-2 text-purple-300 cursor-pointer hover:bg-purple-500 hover:text-purple-200">
+        <Link href="/" className="mr-2 border-2 border-purple-500 rounded-full px-3 py-2 text-purple-300 cursor-pointer hover:bg-purple-500 hover:text-purple-200">
+          <img src="./profile_icon.png" alt="profile icon" />
+        </Link>
+
+        {/* should be sign out goes to main page for now */}
+        <Link href="/" className="ml-2 mr-2 border-2 border-purple-500 rounded-full px-3 py-2 text-purple-300 cursor-pointer hover:bg-purple-500 hover:text-purple-200">
           <img src="./exit_icon.png" alt="exit icon" />
-        </button>
+        </Link>
 
       </div>
 
 
-      <div className="h-2 w-full bg-pink-600 flex justify-center content-center rounded-full">
-        <div className="h-1 w-full bg-white rounded-full"> </div>
+      <div className="h-2 w-full bg-pink-600 flex justify-center items-center rounded-full">
+            <div className="h-1 w-full bg-white rounded-full"></div>
       </div>
 
 
 
-      <div className="w-screen bg-gray-200 p-12 flex items-center">
+      <div className="w-screen bg-neutral p-12 flex items-center">
 
         <div className="rounded-full h-20 w-20 bg-white flex-shrink-0 mr-6">
           {/* idk how to add a picture here  */}
         </div>
 
         <div>
-          <h2 className="text-xl font-bold">Username
+          <h2 className="text-xl font-bold text-purple-500">Username
             {/* i also don't know how to make sure that we get the person's actual usernameentered here */}
           </h2>
         </div>
@@ -44,32 +46,69 @@ function Profile() {
 
 
 
-      <div className="w-full h-72 p-6 bg-neutral-800 rounded-lg shadow-lg flex flex-row space-x-3">
-        <div className="w-full h-full bg-neutral-700 p-8 text-center rounded-lg border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md" >
-          <img src="" alt="" />
-          
-        </div>
+          <div className="w-full bg-neutral-800 p-4 rounded-lg shadow-lg flex flex-col sm:flex-row sm:flex-wrap justify-between">
+            
+            <div className="flex self-center w-48 h-48 sm:w-60 sm:h-60 bg-neutral-700 p-8 rounded-lg border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md relative z-10 mb-4 sm:mb-0 sm:mr-4">
+              <img src="" alt="" />
+             
+              <div className='absolute bottom-4 left-2 flex items-end'>
+                
+                <div className='flex flex-col'>
+                  <div className="self-start m-0 text-sm">
+                    {/* the 'Title' placeholder gets replaced with actual title  */}
+                    Title:</div>
+                  <div className="self-start m-0 text-xs">Description:</div>
 
-        <div className="w-full h-full bg-neutral-700 p-8 text-center rounded-lg  border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md" >
-          <img src="" alt="" />
-          
-        </div>
+                </div>
+              </div>
 
-        <div className="w-full h-full bg-neutral-700 p-8 text-center rounded-lg  border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md" >
-          <img src="" alt="" />
-          
-        </div>
+            </div>
 
-        <div className="w-full h-full bg-neutral-700 p-8 text-center rounded-lg  border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md" >
-          <img src="" alt="" />
-          
-        </div>
+            <div className="flex self-center w-48 h-48 sm:w-60 sm:h-60 bg-neutral-700 p-8 rounded-lg border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md relative z-10 mb-4 sm:mb-0 sm:mr-4">
+              <img src="" alt="" />
+             
+              <div className='absolute bottom-4 left-2 flex items-end'>
+                
+                <div className='flex flex-col'>
+                  <div className="self-start m-0 text-sm">
+                    {/* the 'Title' placeholder gets replaced with actual title  */}
+                    Title:</div>
+                  <div className="self-start m-0 text-xs">Description:</div>
 
-        <div className="w-full h-full bg-neutral-700 p-8 text-center rounded-lg  border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md" >
-         <img src="" alt="" />
-          
-        </div>
-      </div>
+                </div>
+              </div>
+
+            </div>            <div className="flex self-center w-48 h-48 sm:w-60 sm:h-60 bg-neutral-700 p-8 rounded-lg border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md relative z-10 mb-4 sm:mb-0 sm:mr-4">
+              <img src="" alt="" />
+             
+              <div className='absolute bottom-4 left-2 flex items-end'>
+                
+                <div className='flex flex-col'>
+                  <div className="self-start m-0 text-sm">
+                    {/* the 'Title' placeholder gets replaced with actual title  */}
+                    Title:</div>
+                  <div className="self-start m-0 text-xs">Description:</div>
+
+                </div>
+              </div>
+
+            </div>            <div className="flex self-center w-48 h-48 sm:w-60 sm:h-60 bg-neutral-700 p-8 rounded-lg border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md relative z-10 mb-4 sm:mb-0 sm:mr-4">
+              <img src="" alt="" />
+             
+              <div className='absolute bottom-4 left-2 flex items-end'>
+                
+                <div className='flex flex-col'>
+                  <div className="self-start m-0 text-sm">
+                    {/* the 'Title' placeholder gets replaced with actual title  */}
+                    Title:</div>
+                  <div className="self-start m-0 text-xs">Description:</div>
+
+                </div>
+              </div>
+
+            </div>
+
+          </div>
 
     </div>
   )
