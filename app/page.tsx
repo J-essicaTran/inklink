60922a270fc8ce5dc9843b8c30266510aec48178
GreenStripedSweater <reetsuni@gmail.com>
2023-11-30 19:28:31 -0700

    meow is this working

--- app/page.tsx
+++ app/page.tsx
@@ -25,10 +25,59 @@ export default function Home() {
     <html lang="en">
       <body>
         <Header/>
+        {/* remove navigation to test  vv*/}
         <Navigation/>
         <div className="flex flex-col items-center justify-center p-8 pt-20">
           <div className='text-white'>Email: {session?.user?.email}</div>
           <button className='bg-white text-black p-2 px-4 mt-5 rounded-full hover:bg-[#6b7ced]' onClick={() => signOut()}>Logout</button>
+        
+          <div className="w-screen bg-gray-200 p-12 flex flex-col sm:flex-row items-start justify-between">
+            
+            {/* turn them into buttons */}
+            <div className="rounded-xl p-2 text-lg font-bold bg-blue-600 mb-2 sm:mb-0 sm:mr-4 sm:self-end">Explore</div>
+            <div className="rounded-xl p-1 text-sm text-right bg-pink-300 sm:self-start">See more</div>
+          </div>
+
+
+
+
+
+
+
+      <div className="h-2 w-full bg-pink-600 flex justify-center items-center rounded-full">
+        <div className="h-1 w-3/4 bg-white rounded-full"></div>
+      </div>
+
+
+      <div className="w-full h-72 p-6 bg-neutral-800 rounded-lg shadow-lg flex flex-row space-x-3">
+
+
+        <div className="w-full h-full bg-neutral-700 p-8 text-center rounded-lg border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md" >
+          <img src="" alt="" />
+        </div>
+
+        <div className="w-full h-full bg-neutral-700 p-8 text-center rounded-lg  border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md" >
+          <img src="" alt="" />
+        </div>
+
+        <div className="w-full h-full bg-neutral-700 p-8 text-center rounded-lg  border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md" >
+          <img src="" alt="" />
+        </div>
+
+        <div className="w-full h-full bg-neutral-700 p-8 text-center rounded-lg  border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md" >
+          <img src="" alt="" />
+        </div>
+
+        <div className="w-full h-full bg-neutral-700 p-8 text-center rounded-lg  border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md" >
+          <img src="" alt="" />
+        </div>
+      </div>
+
+
+        
+        
+        
+        
         </div>
       </body>
     </html>
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,6 +1,77 @@
+
 function Profile() {
   return (
-    <div>Profile</div>
+    <div>
+      <div className="p-1 pb-3 flex flex-row">
+        <div className="self-center flex-grow">
+          <button className="pl-1">
+            <img src="./name.png" alt="InkLink" />
+          </button>
+        </div>
+
+        <button type="button" className="mr-2 border-2 border-purple-500 rounded-full px-3 py-2 text-purple-300 cursor-pointer hover:bg-purple-500 hover:text-purple-200">
+          <img src="./profile_icon.png" alt="profile icon" />
+        </button>
+
+
+        {/* change to explorer page icon - make sure it takes us to the explorer page  */}
+        <button type="button" className="ml-2 mr-2 border-2 border-purple-500 rounded-full px-3 py-2 text-purple-300 cursor-pointer hover:bg-purple-500 hover:text-purple-200">
+          <img src="./exit_icon.png" alt="exit icon" />
+        </button>
+
+      </div>
+
+
+      <div className="h-2 w-full bg-pink-600 flex justify-center content-center rounded-full">
+        <div className="h-1 w-full bg-white rounded-full"> </div>
+      </div>
+
+
+
+      <div className="w-screen bg-gray-200 p-12 flex items-center">
+
+        <div className="rounded-full h-20 w-20 bg-white flex-shrink-0 mr-6">
+          {/* idk how to add a picture here  */}
+        </div>
+
+        <div>
+          <h2 className="text-xl font-bold">Username
+            {/* i also don't know how to make sure that we get the person's actual usernameentered here */}
+          </h2>
+        </div>
+
+      </div>
+
+
+
+      <div className="w-full h-72 p-6 bg-neutral-800 rounded-lg shadow-lg flex flex-row space-x-3">
+        <div className="w-full h-full bg-neutral-700 p-8 text-center rounded-lg border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md" >
+          <img src="" alt="" />
+          
+        </div>
+
+        <div className="w-full h-full bg-neutral-700 p-8 text-center rounded-lg  border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md" >
+          <img src="" alt="" />
+          
+        </div>
+
+        <div className="w-full h-full bg-neutral-700 p-8 text-center rounded-lg  border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md" >
+          <img src="" alt="" />
+          
+        </div>
+
+        <div className="w-full h-full bg-neutral-700 p-8 text-center rounded-lg  border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md" >
+          <img src="" alt="" />
+          
+        </div>
+
+        <div className="w-full h-full bg-neutral-700 p-8 text-center rounded-lg  border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md" >
+         <img src="" alt="" />
+          
+        </div>
+      </div>
+
+    </div>
   )
 }
 
