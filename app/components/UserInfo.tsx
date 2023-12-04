50fd562012affebd16c621dbb4c379e88d90dde1
J-essicaTran <jessica.tran3@ucalgary.ca>
2023-12-04 04:32:40 +0000

    Fixed html formatting

--- app/components/UserInfo.tsx
+++ app/components/UserInfo.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 function UserInfo({userInfo} : {userInfo:any}) {
   return (
     <div>
-        <p className="text-3xl font-bold text-purple-500">{userInfo.username}</p>
-        <p>{userInfo.email}</p>
+      <p className="text-3xl font-bold text-purple-500">{userInfo.username}</p>
+      <p>{userInfo.email}</p>
     </div>
   )
 }
