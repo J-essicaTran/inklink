ec687dba5a6d32e749a9efef7f5c8c7ed477b00a
J-essicaTran <jessica.tran3@ucalgary.ca>
2023-12-04 05:31:31 +0000

    Updated styling on the journal components in the home page

--- app/page.tsx
+++ app/page.tsx
@@ -44,58 +44,70 @@ export default function Home() {
             <div className="h-1 w-full bg-white rounded-full"></div>
           </div>
           
-          <div className="w-full bg-neutral-800 p-4 rounded-lg shadow-lg flex flex-col sm:flex-row sm:flex-wrap justify-between">
-            <div className="flex self-center w-48 h-48 sm:w-60 sm:h-60 bg-neutral-700 p-8 rounded-lg border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md relative z-10 mb-4 sm:mb-0 sm:mr-4">
-              <img src="" alt="" />
-              <div className='absolute bottom-4 left-2 flex items-end'>
-                <div className='flex flex-col'>
-                  <div className="self-start m-0 text-sm">
-                    {/* the 'Title' placeholder gets replaced with actual title  */}
-                    Title:
-                  </div>
-                  <div className="self-start m-0 text-xs">Description:</div>
-                </div>
+          <div className="w-full bg-neutral-800 p-5 rounded-lg shadow-lg flex flex-col sm:flex-row sm:flex-wrap justify-between gap-5">
+            <div className="max-w-[18rem] rounded-lg shadow bg-neutral-700 border-2">
+              <div className="bg-neutral-800 h-48 rounded-t-lg"></div>
+              <div className="p-5">
+                  <a href="#">
+                      <h5 className="mb-2 text-2xl font-bold text-purple-500">Title</h5>
+                  </a>
+                  <p className="mb-3 text-sm text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                  <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-semibold text-center text-white bg-purple-500 rounded-lg hover:bg-purple-600">
+                      Read more
+                      <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                      </svg>
+                  </a>
               </div>
             </div>
 
-            <div className="flex self-center w-48 h-48 sm:w-60 sm:h-60 bg-neutral-700 p-8 rounded-lg border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md relative z-10 mb-4 sm:mb-0 sm:mr-4">
-              <img src="" alt="" />
-              <div className='absolute bottom-4 left-2 flex items-end'>
-                <div className='flex flex-col'>
-                  <div className="self-start m-0 text-sm">
-                    {/* the 'Title' placeholder gets replaced with actual title  */}
-                    Title:
-                  </div>
-                  <div className="self-start m-0 text-xs">Description:</div>
-                </div>
+            <div className="max-w-[18rem] rounded-lg shadow bg-neutral-700 border-2">
+              <div className="bg-neutral-800 h-48 rounded-t-lg"></div>
+              <div className="p-5">
+                  <a href="#">
+                      <h5 className="mb-2 text-2xl font-bold text-purple-500">Title</h5>
+                  </a>
+                  <p className="mb-3 text-sm text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                  <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-semibold text-center text-white bg-purple-500 rounded-lg hover:bg-purple-600">
+                      Read more
+                      <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                      </svg>
+                  </a>
               </div>
             </div>
 
-            <div className="flex self-center w-48 h-48 sm:w-60 sm:h-60 bg-neutral-700 p-8 rounded-lg border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md relative z-10 mb-4 sm:mb-0 sm:mr-4">
-              <img src="" alt="" />
-              <div className='absolute bottom-4 left-2 flex items-end'>
-                <div className='flex flex-col'>
-                  <div className="self-start m-0 text-sm">
-                    {/* the 'Title' placeholder gets replaced with actual title  */}
-                    Title:
-                  </div>
-                  <div className="self-start m-0 text-xs">Description:</div>
-                </div>
+            <div className="max-w-[18rem] rounded-lg shadow bg-neutral-700 border-2">
+              <div className="bg-neutral-800 h-48 rounded-t-lg"></div>
+              <div className="p-5">
+                  <a href="#">
+                      <h5 className="mb-2 text-2xl font-bold text-purple-500">Title</h5>
+                  </a>
+                  <p className="mb-3 text-sm text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                  <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-semibold text-center text-white bg-purple-500 rounded-lg hover:bg-purple-600">
+                      Read more
+                      <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                      </svg>
+                  </a>
               </div>
             </div>
 
-            <div className="flex self-center w-48 h-48 sm:w-60 sm:h-60 bg-neutral-700 p-8 rounded-lg border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md relative z-10 mb-4 sm:mb-0 sm:mr-4">
-              <img src="" alt="" />
-              <div className='absolute bottom-4 left-2 flex items-end'>
-                <div className='flex flex-col'>
-                  <div className="self-start m-0 text-sm">
-                    {/* the 'Title' placeholder gets replaced with actual title  */}
-                    Title:
-                  </div>
-                  <div className="self-start m-0 text-xs">Description:</div>
-                </div>
+            <div className="max-w-[18rem] rounded-lg shadow bg-neutral-700 border-2">
+              <div className="bg-neutral-800 h-48 rounded-t-lg"></div>
+              <div className="p-5">
+                  <a href="#">
+                      <h5 className="mb-2 text-2xl font-bold text-purple-500">Title</h5>
+                  </a>
+                  <p className="mb-3 text-sm text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                  <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-semibold text-center text-white bg-purple-500 rounded-lg hover:bg-purple-600">
+                      Read more
+                      <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                      </svg>
+                  </a>
               </div>
-            </div>
+            </div>       
           </div>
         </div>
         
@@ -113,58 +125,70 @@ export default function Home() {
             <div className="h-1 w-full bg-white rounded-full"></div>
           </div>
 
-          <div className="w-full bg-neutral-800 p-4 rounded-lg shadow-lg flex flex-col sm:flex-row sm:flex-wrap justify-between">
-            <div className="flex self-center w-48 h-48 sm:w-60 sm:h-60 bg-neutral-700 p-8 rounded-lg border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md relative z-10 mb-4 sm:mb-0 sm:mr-4">
-              <img src="" alt="" />
-              <div className='absolute bottom-4 left-2 flex items-end'>
-                <div className='flex flex-col'>
-                  <div className="self-start m-0 text-sm">
-                    {/* the 'Title' placeholder gets replaced with actual title  */}
-                    Title:
-                  </div>
-                  <div className="self-start m-0 text-xs">Description:</div>
-                </div>
+          <div className="w-full bg-neutral-800 p-5 rounded-lg shadow-lg flex flex-col sm:flex-row sm:flex-wrap justify-between gap-5">
+            <div className="max-w-[18rem] rounded-lg shadow bg-neutral-700 border-2">
+              <div className="bg-neutral-800 h-48 rounded-t-lg"></div>
+              <div className="p-5">
+                  <a href="#">
+                      <h5 className="mb-2 text-2xl font-bold text-purple-500">Title</h5>
+                  </a>
+                  <p className="mb-3 text-sm text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                  <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-semibold text-center text-white bg-purple-500 rounded-lg hover:bg-purple-600">
+                      Read more
+                      <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                      </svg>
+                  </a>
               </div>
             </div>
 
-            <div className="flex self-center w-48 h-48 sm:w-60 sm:h-60 bg-neutral-700 p-8 rounded-lg border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md relative z-10 mb-4 sm:mb-0 sm:mr-4">
-              <img src="" alt="" />
-              <div className='absolute bottom-4 left-2 flex items-end'>
-                <div className='flex flex-col'>
-                  <div className="self-start m-0 text-sm">
-                    {/* the 'Title' placeholder gets replaced with actual title  */}
-                    Title:
-                  </div>
-                  <div className="self-start m-0 text-xs">Description:</div>
-                </div>
+            <div className="max-w-[18rem] rounded-lg shadow bg-neutral-700 border-2">
+              <div className="bg-neutral-800 h-48 rounded-t-lg"></div>
+              <div className="p-5">
+                  <a href="#">
+                      <h5 className="mb-2 text-2xl font-bold text-purple-500">Title</h5>
+                  </a>
+                  <p className="mb-3 text-sm text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                  <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-semibold text-center text-white bg-purple-500 rounded-lg hover:bg-purple-600">
+                      Read more
+                      <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                      </svg>
+                  </a>
               </div>
             </div>
 
-            <div className="flex self-center w-48 h-48 sm:w-60 sm:h-60 bg-neutral-700 p-8 rounded-lg border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md relative z-10 mb-4 sm:mb-0 sm:mr-4">
-              <img src="" alt="" />
-              <div className='absolute bottom-4 left-2 flex items-end'>
-                <div className='flex flex-col'>
-                  <div className="self-start m-0 text-sm">
-                    {/* the 'Title' placeholder gets replaced with actual title  */}
-                    Title:
-                  </div>
-                  <div className="self-start m-0 text-xs">Description:</div>
-                </div>
+            <div className="max-w-[18rem] rounded-lg shadow bg-neutral-700 border-2">
+              <div className="bg-neutral-800 h-48 rounded-t-lg"></div>
+              <div className="p-5">
+                  <a href="#">
+                      <h5 className="mb-2 text-2xl font-bold text-purple-500">Title</h5>
+                  </a>
+                  <p className="mb-3 text-sm text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                  <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-semibold text-center text-white bg-purple-500 rounded-lg hover:bg-purple-600">
+                      Read more
+                      <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                      </svg>
+                  </a>
               </div>
             </div>
 
-            <div className="flex self-center w-48 h-48 sm:w-60 sm:h-60 bg-neutral-700 p-8 rounded-lg border-2 border-gray-300 hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md relative z-10 mb-4 sm:mb-0 sm:mr-4">
-              <img src="" alt="" />
-              <div className='absolute bottom-4 left-2 flex items-end'>
-                <div className='flex flex-col'>
-                  <div className="self-start m-0 text-sm">
-                    {/* the 'Title' placeholder gets replaced with actual title  */}
-                    Title:
-                  </div>
-                  <div className="self-start m-0 text-xs">Description:</div>
-                </div>
+            <div className="max-w-[18rem] rounded-lg shadow bg-neutral-700 border-2">
+              <div className="bg-neutral-800 h-48 rounded-t-lg"></div>
+              <div className="p-5">
+                  <a href="#">
+                      <h5 className="mb-2 text-2xl font-bold text-purple-500">Title</h5>
+                  </a>
+                  <p className="mb-3 text-sm text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                  <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-semibold text-center text-white bg-purple-500 rounded-lg hover:bg-purple-600">
+                      Read more
+                      <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                      </svg>
+                  </a>
               </div>
-            </div>
+            </div>       
           </div>
         </div>
       </div>
