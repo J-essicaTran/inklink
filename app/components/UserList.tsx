2c6606539ef3bfd6e7f56e7df07af8c6401459c5
J-essicaTran <jessica.tran3@ucalgary.ca>
2023-12-04 23:30:18 -0700

    Styled search user page

--- app/components/UserList.tsx
+++ app/components/UserList.tsx
@@ -1,7 +1,8 @@
 'use client'
 import { collection } from 'firebase/firestore';
 import { useCollection } from 'react-firebase-hooks/firestore';
-import { db, firestoreDB } from '../firebase';
+import { firestoreDB } from '../firebase';
+import { FaUserPlus } from "react-icons/fa";
 
 function UserList() {
 
@@ -16,11 +17,22 @@ function UserList() {
     }
 
   return (
-    <div>
+    <div className="m-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
         {data?.docs.map((doc) => (
-            <div key={doc.id}>
-                <h2>{doc.data().username}</h2>
-                <p>Email: {doc.data().email}</p>
+            <div key={doc.id} className="flex flex-col mb-5 p-5 bg-neutral-800 rounded-lg border-2 border-white hover:border-pink-600 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-md">
+                <div className="flex mb-10">
+                    <div className="border-2 border-white bg-gradient-to-b from-[#BA6CEB] to-[#4D5ECF] rounded-full p-4 mr-5 flex-shrink-0">
+                        <img className="w-12 h-12 object-cover" src="/logo.png" alt="logo" />
+                    </div>
+                    <div className="flex flex-col">
+                        <p className="text-3xl font-bold text-purple-500">{doc.data().username}</p>
+                        <p>{doc.data().email}</p>
+                    </div>
+                </div>
+                <button className="flex items-center ml-auto w-fit bg-[#BA6CEB] hover:bg-[#b051eb] py-1.5 px-8 rounded-full">
+                    <FaUserPlus className="mr-2"/>
+                    <p className="text-white font-semibold">Follow</p>
+                </button>
             </div>
         ))}
     </div>
