63b99c234e644dbf05b08423799af4e0581183b5
JohnLugue <lugue.johndale@gmail.com>
2023-12-05 21:01:34 -0700

    Minor styling added to admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -5,7 +5,14 @@ function Admin() {
   return (
     <div>
       <Header />
-      <UserListAdmin />
+
+      <div className="h-2 w-full mt-3 bg-pink-600 flex justify-center items-center rounded-full">
+        <div className="h-1 w-full bg-white rounded-full"></div>
+      </div>
+
+      <div className="h-full w-full rounded-lg p-3 bg-neutral-900">
+        <UserListAdmin />
+      </div>
     </div>
   );
 }
